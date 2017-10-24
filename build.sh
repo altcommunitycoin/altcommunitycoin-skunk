98acba7cb561742b8a8b9bce4a002241daef7d90
altcommunitycoin <31189922+altcommunitycoin@users.noreply.github.com>
2017-10-24 19:01:56 +0200

    updated build.sh for 1click build

--- build.sh
+++ build.sh
@@ -19,19 +19,6 @@ echo "nproc: $NPROC"
 sudo apt-get install build-essential libssl-dev libdb++-dev libdb-dev libboost-all-dev libqrencode-dev libminiupnpc-dev
 sudo apt-get update
 
-# By default, assume running within repo
-repo=$(pwd)
-file=$repo/src/altcommunitycoind
-if [ ! -e "$file" ]; then
-	# Now assume running outside and repo has been downloaded and named altcommunitycoin
-	if [ ! -e "$repo/altcommunitycoin/build.sh" ]; then
-		# if not, download the repo and name it altcommunitycoin
-		git clone https://github.com/altcommunitycoin/altcommunitycoin-skunk altcommunitycoin
-	fi
-	repo=$repo/altcommunitycoin
-	file=$repo/src/altcommunitycoind
-	cd $repo/src/
-fi
 make -j$NPROC -f makefile.unix
 
 cp $repo/src/altcommunitycoind /usr/bin/altcommunitycoind
